e2e996483857c68846dd2ad82c45cc14d0fabd30
Laith Harb <harblaith77@gmail.com>
2023-07-08 07:15:48 -0400

    3.3-state-and-event-handling

--- 01-counter/my-counter/src/App.jsx
+++ 01-counter/my-counter/src/App.jsx
@@ -1,34 +1,29 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
 
+  const addToCount = () => {
+    if (count < 10) {
+      setCount(count + 1);
+    }
+    console.log(count);
+  };
+
+  const subtractFromCount = () => {
+    if (count > 0) {
+      setCount(count - 1);
+    }
+  };
+
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React + Whatever</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <h4>The current count is...</h4>
+      <h1>{count}</h1>
+      <button onClick={subtractFromCount}>-</button>
+      <button onClick={addToCount}>+</button>
+    </div>
   );
 }
 
