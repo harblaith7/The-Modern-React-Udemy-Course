a9e8f45da3ecbf9862e8babdd6d35d7fc9cbaf60
Laith Harb <harblaith77@gmail.com>
2023-07-08 06:09:58 -0400

    2.4-build-counter-with-react-app

---
+++ 01-counter/counter-with-react/src/App.jsx
@@ -0,0 +1,29 @@
+import "./App.css";
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  const addToCount = () => {
+    if (count < 10) {
+      setCount(count + 1);
+    }
+  };
+
+  const subtractFromCount = () => {
+    if (count > 0) {
+      setCount(count - 1);
+    }
+  };
+
+  return (
+    <div>
+      <h4>The current count is...</h4>
+      <h1>{count}</h1>
+      <button onClick={subtractFromCount}>-</button>
+      <button onClick={addToCount}>+</button>
+    </div>
+  );
+}
+
+export default App;
