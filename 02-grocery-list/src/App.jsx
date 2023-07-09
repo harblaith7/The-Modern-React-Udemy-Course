767fe5916719b074c390a1a732a22d5345fa683e
Laith Harb <harblaith77@gmail.com>
2023-07-08 20:16:30 -0400

    4.3-building-the-jsx

--- 02-grocery-list/src/App.jsx
+++ 02-grocery-list/src/App.jsx
@@ -1,10 +1,35 @@
 import "./App.css";
+import groceryCartImg from "./assets/grocery-cart.png";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
+    <main className="App">
+      <div>
+        <div>
+          <h4 className="success">You're Done</h4>
+          <div className="header">
+            <h1>Shopping List</h1>
+            <img src={groceryCartImg} alt="" />
+            <input
+              type="text"
+              placeholder="Add an Item"
+              className="item-input"
+            />
+          </div>
+        </div>
+        <ul>
+          <li>
+            <div className="container">
+              <input type="checkbox" />
+              <p>Carrots</p>
+            </div>
+            <div>
+              <button className="remove-button">X</button>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </main>
   );
 }
 
