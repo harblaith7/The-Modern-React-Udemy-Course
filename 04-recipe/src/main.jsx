6fed02186f5ee8d5ab59ea32518396e5e95a6dda
Laith Harb <harblaith77@gmail.com>
2023-07-14 05:10:24 -0400

    5.4-routing-setup

--- 04-recipe/src/main.jsx
+++ 04-recipe/src/main.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import {
+  RouterProvider,
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
 
+const router = createBrowserRouter(
+  createRoutesFromElements(<Route path="/" element={<App />} />)
+);
+
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <RouterProvider router={router} />
 );
