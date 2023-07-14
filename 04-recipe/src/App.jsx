1ae05036e95ba403582cf21bc94d066cf1d81aa0
Laith Harb <harblaith77@gmail.com>
2023-07-14 06:15:39 -0400

    4.7-Link-navigat ion

--- 04-recipe/src/App.jsx
+++ 04-recipe/src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
 import { Outlet } from "react-router-dom";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
       <main className="main_container">
         <Outlet />
       </main>
