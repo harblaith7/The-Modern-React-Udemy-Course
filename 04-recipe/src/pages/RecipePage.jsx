7f4b624172fd9cffa039c05ba6f014b8fa0db873
Laith Harb <harblaith77@gmail.com>
2023-07-14 07:01:47 -0400

    7.9 useParams

--- 04-recipe/src/pages/RecipePage.jsx
+++ 04-recipe/src/pages/RecipePage.jsx
@@ -1,7 +1,13 @@
+import { useParams } from "react-router-dom";
+import { recipes } from "../components/CardList";
+
 export default function RecipePage() {
+  const { id } = useParams();
+
+  const recipe = recipes.find((r) => r.id === parseInt(id));
   return (
     <div>
-      <h1>Poke Bowl</h1>
+      <h1>{recipe.name}</h1>
     </div>
   );
 }
