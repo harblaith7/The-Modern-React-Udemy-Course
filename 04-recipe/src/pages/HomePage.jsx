1afd21b57332d89a91746e19669d2bf610c27a03
Laith Harb <harblaith77@gmail.com>
2023-07-15 10:25:00 -0400

    8.4-custom-hook

--- 04-recipe/src/pages/HomePage.jsx
+++ 04-recipe/src/pages/HomePage.jsx
@@ -1,36 +1,9 @@
 import CardList from "../components/CardList";
 import Header from "../components/Header";
-import axios from "axios";
-import { useEffect, useState } from "react";
-
-const options = {
-  method: "GET",
-  url: "https://tasty.p.rapidapi.com/recipes/list",
-  params: {
-    from: "0",
-    size: "20",
-  },
-  headers: {
-    "X-RapidAPI-Key": "30b594a022mshc657030b09147dcp153ad1jsn8b9a863a5527",
-    "X-RapidAPI-Host": "tasty.p.rapidapi.com",
-  },
-};
+import useFetchRecipes from "../hooks/useFetchRecipes";
 
 export default function HomePage() {
-  const [recipes, setRecipes] = useState([]);
-
-  useEffect(() => {
-    fetchRecipes();
-  }, []);
-
-  const fetchRecipes = async () => {
-    try {
-      const response = await axios.request(options);
-      setRecipes(response.data.results);
-    } catch (error) {
-      console.error(error);
-    }
-  };
+  const [recipes] = useFetchRecipes();
 
   return (
     <>
