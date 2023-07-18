3da224bac48cb9ec44db343eee7680f3c7b0a850
Laith Harb <harblaith77@gmail.com>
2023-07-18 08:04:47 -0400

    10.1 new react app

--- 04-recipe/src/main.jsx
+++ 04-recipe/src/main.jsx
@@ -31,3 +31,7 @@ const router = createBrowserRouter(
 ReactDOM.createRoot(document.getElementById("root")).render(
   <RouterProvider router={router} />
 );
+
+// useState
+// useEffect
+// useReducer
