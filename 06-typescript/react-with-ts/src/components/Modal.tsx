13c741ab1cd32a83830e60530120544e24b66c22
Laith Harb <harblaith77@gmail.com>
2023-07-23 16:23:37 -0400

    11.8 packages and types

--- 06-typescript/react-with-ts/src/components/Modal.tsx
+++ 06-typescript/react-with-ts/src/components/Modal.tsx
@@ -1,9 +1,29 @@
 import { useState } from "react";
+import { Gift } from "../App";
+import { v4 as uuid } from "uuid";
 
-export default function Modal() {
+interface ModalProps {
+  onClose: () => void;
+  onSave: (gift: Gift) => void;
+}
+
+export default function Modal({
+  onClose: handleClose,
+  onSave: handleSave,
+}: ModalProps) {
   const [name, setName] = useState("");
   const [value, setValue] = useState("");
   const [image, setImage] = useState("");
+
+  const saveGift = () => {
+    if (name && value && image) {
+      handleSave({ id: uuid(), name, value: parseFloat(value), image });
+    }
+    setImage("");
+    setName("");
+    setValue("");
+    handleClose();
+  };
 
   return (
     <div className="backdrop">
@@ -24,8 +44,8 @@ export default function Modal() {
           value={image}
           onChange={(e) => setImage(e.target.value)}
         />
-        <button>Save</button>
-        <button>Close</button>
+        <button onClick={saveGift}>Save</button>
+        <button onClick={handleClose}>Close</button>
       </div>
     </div>
   );
