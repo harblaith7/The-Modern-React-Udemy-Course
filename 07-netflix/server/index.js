219a33fb69c28e0336b1037a56696fe7717c5fdb
Laith Harb <harblaith77@gmail.com>
2023-09-05 08:02:13 -0400

    13.04 movies endpoint

--- 07-netflix/server/index.js
+++ 07-netflix/server/index.js
@@ -1,9 +1,13 @@
 const express = require("express");
-
+const movies = require("./movies.json");
 const app = express();
 
 app.get("/", (req, res) => {
   return res.send("HELLO WORLD");
+});
+
+app.get("/movies/list", (req, res) => {
+  return res.send(movies);
 });
 
 app.listen(8080, () => {
