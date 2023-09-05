08378bd12bc7e8da43f429a1648412b802d26094
Laith Harb <harblaith77@gmail.com>
2023-09-05 16:04:39 -0400

    13.10 navigate to watch page

--- 07-netflix/client/src/components/MovieCard.tsx
+++ 07-netflix/client/src/components/MovieCard.tsx
@@ -1,9 +1,12 @@
 import { ChevronDownIcon } from "@heroicons/react/24/outline";
 import { PlayIcon } from "@heroicons/react/24/solid";
+import { useNavigate } from "react-router-dom";
 import { Movie } from "../types";
 
 const MovieCard = ({ movie }: { movie: Movie }) => {
-  const { thumbnailUrl, description, duration, title, genre } = movie;
+  const { thumbnailUrl, id, description, duration, title, genre } = movie;
+
+  const navigate = useNavigate();
 
   return (
     <div className="group bg-zinc-900 col-span relative h-[12vw] w-[24%]">
@@ -73,9 +76,12 @@ const MovieCard = ({ movie }: { movie: Movie }) => {
           "
         >
           <div className="flex flex-row items-center gap-3">
-            <div className="cursor-pointer w-6 h-6 lg:w-10 lg:h-10 bg-white rounded-full flex justify-center items-center transition hover:bg-neutral-300">
+            <button
+              className="cursor-pointer w-6 h-6 lg:w-10 lg:h-10 bg-white rounded-full flex justify-center items-center transition hover:bg-neutral-300"
+              onClick={() => navigate(`/browse/watch/${id}`)}
+            >
               <PlayIcon className="text-black w-4 lg:w-6" />
-            </div>
+            </button>
             <div className="cursor-pointer ml-auto group/item w-6 h-6 lg:w-10 lg:h-10 border-white border-2 rounded-full flex justify-center items-center transition hover:border-neutral-300">
               <ChevronDownIcon className="text-white group-hover/item:text-neutral-300 w-4 lg:w-6" />
             </div>
