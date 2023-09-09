6dc5b43691b8ef7db5f2556c68491db1f6b86297
Laith Harb <harblaith77@gmail.com>
2023-09-09 09:20:15 -0400

    15.6 refactor endpoint

--- 07-netflix/server/index.js
+++ 07-netflix/server/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
-const movies = require("./movies.json");
 const cors = require("cors");
 const app = express();
+const { prisma } = require("./db");
 
 app.use(cors());
 
@@ -9,19 +9,23 @@ app.get("/", (req, res) => {
   return res.send("HELLO WORLD");
 });
 
-app.get("/movies/list", (req, res) => {
+app.get("/movies/list", async (req, res) => {
   const offset = parseInt(req.query.offset);
-  const from = offset;
-  const to = from + 12;
-  const moviesSubset = [...movies].slice(from, to);
-  setTimeout(() => {
-    return res.json({ movies: moviesSubset, count: movies.length });
-  }, 3000);
+  const count = await prisma.movie.count();
+  const movies = await prisma.movie.findMany({
+    take: 12,
+    skip: offset,
+  });
+  return res.json({ movies, count });
 });
 
-app.get("/movie/:id", (req, res) => {
+app.get("/movie/:id", async (req, res) => {
   const id = req.params.id;
-  const movie = movies.find((m) => m.id === id);
+  const movie = await prisma.movie.findUnique({
+    where: {
+      id: parseInt(id),
+    },
+  });
   return res.send(movie);
 });
 
