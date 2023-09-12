5d25e9e84eed35d7ec69897c923986a189034cf0
Laith Harb <harblaith77@gmail.com>
2023-09-12 06:31:41 -0400

    17.4 useform hook

--- 07-netflix/client/src/pages/LoginPage.tsx
+++ 07-netflix/client/src/pages/LoginPage.tsx
@@ -1,6 +1,13 @@
 import NavBar from "../components/NavBar";
 import Input from "../components/Input";
 import { useState } from "react";
+import { useForm, SubmitHandler } from "react-hook-form";
+
+type Inputs = {
+  email: string;
+  name: string;
+  password: string;
+};
 
 enum Variant {
   SIGN_UP,
@@ -8,7 +15,17 @@ enum Variant {
 }
 
 export default function LoginPage() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    watch,
+  } = useForm<Inputs>();
   const [variant, setVariant] = useState(Variant.LOGIN_IN);
+
+  const onSubmit: SubmitHandler<Inputs> = (data) => {
+    console.log(data);
+  };
 
   return (
     <div className="relative bg-black h-screen w-screen bg-opacity-50">
@@ -16,7 +33,10 @@ export default function LoginPage() {
       <div className="flex justify-center items-center h-full">
         <div className="bg-black bg-opacity-70 p-16 self-center mt-2 w-full max-w-md rounded-md">
           <h2 className="text-white text-4xl mb-8 font-semibold">Sign in</h2>
-          <form className="flex flex-col gap-4">
+          <form
+            className="flex flex-col gap-4"
+            onSubmit={handleSubmit(onSubmit)}
+          >
             {variant === Variant.SIGN_UP && (
               <Input id="username" type="text" label="Username" />
             )}
