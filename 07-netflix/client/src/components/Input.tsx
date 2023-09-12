1269e016be8d406222a6169f285b4bb4ee3f0ba9
Laith Harb <harblaith77@gmail.com>
2023-09-12 06:16:23 -0400

    17.2 form

--- 07-netflix/client/src/components/Input.tsx
+++ 07-netflix/client/src/components/Input.tsx
@@ -1,7 +1,17 @@
-export default function Input() {
+interface InputProps {
+  id: string;
+  label: string;
+  type?: string;
+}
+
+export default function Input({ id, label, type }: InputProps) {
   return (
     <div className="relative">
-      <input className="block rounded-md px-6 pt-6 pb-1 w-full text-md text-white bg-neutral-700 appearance-none focus:outline-none focus:ring-0 peer invalid:border-b-1" />
+      <input
+        id={id}
+        type={type}
+        className="block rounded-md px-6 pt-6 pb-1 w-full text-md text-white bg-neutral-700 appearance-none focus:outline-none focus:ring-0 peer invalid:border-b-1"
+      />
       <label
         className="absolute 
           text-md
@@ -19,7 +29,7 @@ export default function Input() {
           peer-focus:scale-75
           peer-focus:-translate-y-3"
       >
-        Email
+        {label}
       </label>
     </div>
   );
--- 07-netflix/client/src/pages/LoginPage.tsx
+++ 07-netflix/client/src/pages/LoginPage.tsx
@@ -1,7 +1,15 @@
 import NavBar from "../components/NavBar";
 import Input from "../components/Input";
+import { useState } from "react";
+
+enum Variant {
+  SIGN_UP,
+  LOGIN_IN,
+}
 
 export default function LoginPage() {
+  const [variant, setVariant] = useState(Variant.LOGIN_IN);
+
   return (
     <div className="relative bg-black h-screen w-screen bg-opacity-50">
       <NavBar />
@@ -9,17 +17,35 @@ export default function LoginPage() {
         <div className="bg-black bg-opacity-70 p-16 self-center mt-2 w-full max-w-md rounded-md">
           <h2 className="text-white text-4xl mb-8 font-semibold">Sign in</h2>
           <form className="flex flex-col gap-4">
-            <Input />
+            {variant === Variant.SIGN_UP && (
+              <Input id="username" type="text" label="Username" />
+            )}
+            <Input id="email" type="email" label="Email address" />
+            <Input id="password" type="password" label="Password" />
             <input
               type="submit"
               className="bg-red-400 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700"
             />
           </form>
-          <p className="text-neutral-500 mt-12">
-            <span className="text-white ml-1 hover:underline cursor-pointer">
-              First time using Netflix?
-            </span>
-          </p>
+          {variant === Variant.LOGIN_IN ? (
+            <p
+              className="text-neutral-500 mt-12"
+              onClick={() => setVariant(Variant.SIGN_UP)}
+            >
+              <span className="text-white ml-1 hover:underline cursor-pointer">
+                First time using Netflix?
+              </span>
+            </p>
+          ) : (
+            <p
+              className="text-neutral-500 mt-12"
+              onClick={() => setVariant(Variant.LOGIN_IN)}
+            >
+              <span className="text-white ml-1 hover:underline cursor-pointer">
+                Already have an account?
+              </span>
+            </p>
+          )}
         </div>
       </div>
     </div>
