19f0147f7539521c42e9a2c67026590adcc0f502
Laith Harb <harblaith77@gmail.com>
2023-09-16 11:13:06 -0400

    18.02 useAuth

---
+++ 07-netflix/client/src/hooks/useAuth.ts
@@ -0,0 +1,40 @@
+import axios from "axios";
+
+const useAuth = () => {
+  const login = async ({
+    email,
+    password,
+  }: {
+    email: string;
+    password: string;
+  }) => {
+    const response = await axios.post("http://localhost:8080/auth/login", {
+      email,
+      password,
+    });
+    return response.data;
+  };
+
+  const signup = ({
+    email,
+    password,
+    username,
+  }: {
+    email: string;
+    password: string;
+    username: string;
+  }) => {
+    const response = await axios.post("http://localhost:8080/auth/signup", {
+      email,
+      password,
+      username,
+    });
+    return response.data;
+  };
+
+  const fetchUser = () => {};
+
+  return { signup, login, fetchUser };
+};
+
+export default useAuth;
